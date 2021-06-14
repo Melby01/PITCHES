334a2aae9b0f86de8adc15e0eaca6db988a7cd9d
kelvinhovinho <melby.okozi@student.moringaschool.com>
2021-06-14 21:27:49 +0300

    Update all changes done so far

---
+++ app/main/views.py
@@ -0,0 +1,121 @@
+from flask import render_template,request,redirect,url_for,abort
+from ..models import *
+from . import main
+from .forms import *
+from .. import db,photos
+from flask_login import login_required,current_user
+import markdown2 
+
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - Choose your favorite pitch'
+    return render_template('index.html', title = title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/pitch/comment/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_comment(id):
+    form = CommentForm()
+    pitch= get_pitch(id)
+    if form.validate_on_submit():
+        title = form.title.data
+        comment = form.comment.data
+
+        # Updated comment instance
+        new_comment = Comment(pitch_id=pitch.id,pitch_title=title,image_path=pitch.poster,pitch_comment=comment,user=current_user)
+
+        # save comment method
+        new_comment.save_comment()
+        return redirect(url_for('.pitch',id = pitch.id ))
+
+    title = f'{pitch.title} comment'
+    return render_template('new_comment.html',title = title, comment_form=form, pitch=pitch)
+
+@main.route('/categories/<cate>')
+def category(cate):
+    '''
+    function to return the pitches by category
+    '''
+    category = Pitches.get_pitches(cate)
+    
+    
+    # print(category)
+    title = f'{cate}'
+    return render_template('categories.html',title = title, category = category)
+
+@main.route('/pitch/', methods = ['GET','POST'])
+@login_required
+def new_pitch():
+
+    form = PitchForm()
+
+    if form.validate_on_submit():
+        category = form.category.data
+        pitch= form.pitch.data
+        title=form.title.data
+
+        # Updated pitchinstance
+        new_pitch = Pitches(title=title,category= category,pitch= pitch,user_id=current_user.id)
+
+        title='New Pitch'
+
+        new_pitch.save_pitch()
+
+        return redirect(url_for('main.index'))
+
+    return render_template('pitch.html',form= form)
+
+
+
+@main.route('/comment/<int:id>')
+def single_comment(id):
+    comment=Comment.query.get(id)
+    if comment is None:
+        abort(404)
+    format_comment = markdown2.markdown(comment.pitch_comment,extras=["code-friendly", "fenced-code-blocks"])
+    return render_template('comment.html',comment = comment,format_comment=format_comment)
